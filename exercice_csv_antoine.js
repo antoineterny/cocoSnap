3a8e7f77d4f017765badb9344325a423bfba34ff
antoineterny <63339704+antoineterny@users.noreply.github.com>
2020-04-12 11:02:46 +0200

    csv et json marchent

--- exercice_csv_antoine.js
+++ exercice_csv_antoine.js
@@ -6,14 +6,13 @@ const rl = readline.createInterface({
   crlfDelay: Infinity
 });
 
+const rl2 = readline.createInterface({
+  input: fs.createReadStream('deaths_global.txt'),
+  crlfDelay: Infinity
+});
+
 let selectedCountries = ["Brazil", "France", "United Kingdom"]
-
 let lineNumber = 1
-
-// Your goal is to fill into this data object the same structure as the data variable in your graph.js file
-// "Brazil": [
-//      {"date": "3/1/20", "confirmed_cases": "2", "deaths": "0"},
-
 let data = {}
 
 rl.on('line', (line) => {
@@ -39,18 +38,38 @@ rl.on('line', (line) => {
 });
 
 rl.on('close', () => {
-  // The code here runs when all lines of the file were processed
+  console.log('Finished processing confirmed_global.txt ')
+})
 
-  // Here you will replace the console.log for the correct code
-  // to write the variable data into a file named data.json
-  console.log(data)
-  
+rl2.on('line', (line) => {
+  let lineArray = line.split(","); 
+
+  if (lineNumber === 1) {
+    dates = lineArray.slice(4);
+  }
+
+  else {
+    let provinceOfLine = lineArray[0], countryOfLine = lineArray[1];
+    let deaths = lineArray.slice(4);
+
+    if (selectedCountries.includes(countryOfLine) && provinceOfLine === "") { 
+      for (i=0; i<deaths.length; i++){
+        data[countryOfLine][i]["deaths"] = deaths[i];
+      }
+    }
+  }
+
+  lineNumber += 1
+});
+
+
+rl2.on('close', () => {
   fs.writeFile('data.json', (JSON.stringify(data)), err => {
     if (err) {
       console.error(err)
       return
     }
   })
-
+  console.log(data);
   console.log('Finished processing files')
 })
